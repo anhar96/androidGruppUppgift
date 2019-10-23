e73fde9497849cba1834161fb43bbc3a8dd82c6e
anhar shebib <anhar96@hotmail.com>
2019-10-23 10:39:49 +0200

    working version

---
+++ app/src/main/java/com/dalircode/contactslist/ContactLista.java
@@ -0,0 +1,14 @@
+package com.dalircode.contactslist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_lista);
+    }
+}
